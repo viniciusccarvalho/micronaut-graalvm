9d078f12353573d5dc31b4eecffa3ac54bf84069
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2020-02-26 11:19:42 -0500

    added reflection resources

--- src/main/kotlin/micronaut/graal/Application.kt
+++ src/main/kotlin/micronaut/graal/Application.kt
@@ -1,9 +1,10 @@
 package micronaut.graal
 
+import com.google.cloud.sql.postgres.SocketFactory
 import io.micronaut.core.annotation.TypeHint
 import io.micronaut.runtime.Micronaut
 
-@TypeHint(value = [org.postgresql.Driver::class])
+@TypeHint(value = [org.postgresql.Driver::class, SocketFactory::class])
 object Application {
 
     @JvmStatic
